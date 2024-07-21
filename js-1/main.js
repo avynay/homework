d0f33b431b2cc8c9ffd862ec73faedaefae2e5f3
avynay <fotike.pr@gmail.com>
2024-07-21 12:43:41 +0300

    add js-1

---
+++ js-1/main.js
@@ -0,0 +1,18 @@
+let message = 'Hello'
+console.log(message)
+message = 'Hi'
+
+const firstName = 'Andrey'
+const secondName = 'Len'
+console.log(firstName,secondName)
+
+const age = '24'
+console.log(age, 'y.o')
+
+let lazyText = 'i am not lazy,',
+middleWords = 'i am',
+ngrSentence= 'energy efficient'
+console.log(lazyText,middleWords,ngrSentence,)
+
+const lastLine = 'BYE'
+console.log(lastLine)
