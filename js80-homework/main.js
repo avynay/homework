bcc5ca08d30fcc83e209ac4d3462d693a60e316b
avynay <fotike.pr@gmail.com>
2024-07-23 10:37:23 +0300

    add js80-homework

---
+++ js80-homework/main.js
@@ -0,0 +1,20 @@
+for(let i = 2; i <= 12; i = i + 2){
+  console.log(`плюс ${i}`)
+  }
+  const line = '------------------------------------------'
+  console.log(line)
+
+  for(let a = 10; a >= 1; a --){
+    console.log(`минус ${a}`)
+    }
+
+//     let b = 0
+// while(b < 12){
+//   b = b + 2
+//   console.log(b)
+// }
+
+// let c = 11
+// while(c >1){
+//   c --
+//   console.log(c)}
