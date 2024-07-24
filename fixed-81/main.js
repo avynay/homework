f4b19e2ca6992aed5aa0e8323155f3bdad07c79f
avynay <fotike.pr@gmail.com>
2024-07-24 22:20:34 +0300

    add fixed-81

---
+++ fixed-81/main.js
@@ -0,0 +1,10 @@
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let i = 1; i < num.length; i = i+2) {
+	console.log(num[i]);
+}
+console.log('-----------------------')
+let rainbow = ['красный', 'оранжевый', 'желтый', 'зеленый', 'голубой','синий','фиолетовый'];
+for (let i = rainbow.length - 1; i >= 0; i--) {
+	console.log(rainbow[i]);
+}
+
