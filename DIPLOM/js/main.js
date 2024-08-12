ce80de682ef0797810c9706f9b1b0a09f9f3323f
avynay <fotike.pr@gmail.com>
2024-08-12 23:47:20 +0300

    add DIPLOM

---
+++ DIPLOM/js/main.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const swiper = new Swiper('.testimonials__slider', {
+  autoplay: {
+    delay: 5000,
+  },
+  speed:3000,
+  loop: true,
+  initialSlide: 1,
+  slidesPerView: 3.35,
+  spaceBetween: 24,
+  breakpoints: {
+  
+  }
+});
