6aeb995b2f25f232caeef035ce39d125b8ff77e5
avynay <fotike.pr@gmail.com>
2024-08-14 00:18:46 +0300

    add DIPLOM-FOOTER

---
+++ DIPLOM-FOOTER/js/main.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const swiper = new Swiper('.testimonials__slider', {
+  autoplay: {
+    delay: 0 ,
+    pauseOnMouseEnter: true,
+  },
+  speed:2000,
+  loop: true,
+  initialSlide: 1,
+  slidesPerView: 3.35,
+  spaceBetween: 24,
+  breakpoints: {
+  
+  }
+});
